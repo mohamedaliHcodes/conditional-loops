58a5aef9aafd7ddfd06e9b30112c1835e7b186bb
mohamedHcodes <71350735+hassan455@users.noreply.github.com>
2023-02-23 14:49:19 +0300

    Dealing with conditionals and solving common dsa questions

--- src/countNumOfApp.java
+++ src/countNumOfApp.java
@@ -1,2 +1,17 @@
 public class countNumOfApp {
-}
+    public static void main(String[] args) {
+        int num=45536;
+
+       int count = 0;
+       while (num>0){ //while the number is greater than zero
+            int rem=num % 10;//divide through the number by ten and find reminder
+            if(rem==5){
+                count++;
+            }
+            num=num/10;
+    }
+            System.out.println(count);
+        }
+    }
+
+
